2d0e6289be93802acbd2999eb20e5939cf14701d
zzjjcc <zheng.jianchao@iwhalecloud.com>
2025-01-11 13:44:22 +0800

    添加Antlr4学习

--- sb3-test/spring-boot3-phoenix/src/main/java/io/jcz/antlr4/Antlr4Example01.java
+++ sb3-test/spring-boot3-phoenix/src/main/java/io/jcz/antlr4/Antlr4Example01.java
@@ -1,15 +1,64 @@
 package io.jcz.antlr4;
 
-import org.apache.phoenix.parse.SQLParser;
-import org.apache.phoenix.parse.SelectStatement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 public class Antlr4Example01 {
-    public static void main(String[] args) throws SQLException {
-        SQLParser parser = new SQLParser("select * from test");
 
-        SelectStatement selectStatement = parser.parseQuery();
+    static final Logger LOG = LoggerFactory.getLogger(Antlr4Example01.class);
 
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+
+        Antlr4Example01 example = new Antlr4Example01();
+
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        list.add("e");
+        list.add("f");
+        list.add("g");
+        list.add("h");
+        list.add("i");
+        list.add("j");
+
+
+        List<String> res = new ArrayList<>();
+        long start = System.currentTimeMillis();
+        List<CompletableFuture<Void>> futures = new ArrayList<>();
+        for (String s : list) {
+            CompletableFuture<Void> cf = CompletableFuture.runAsync(() -> example.m2(res, s));
+            futures.add(cf);
+            //res.add(s + "<-->");
+        }
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+        long end = System.currentTimeMillis();
+        LOG.info("耗时: {}", end - start);
+        LOG.info("{}",res);
+    }
+
+    public void m1() {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void m2(List<String> res, String s) {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        String ss = s + "<-->";
+        LOG.info("{}", ss);
+        res.add(ss);
     }
 }
