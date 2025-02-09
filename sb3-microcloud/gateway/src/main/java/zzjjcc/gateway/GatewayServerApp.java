c155b0eabfd8b4021da45b43a967281404780a86
zzjjcc <zheng.jianchao@iwhalecloud.com>
2025-02-09 23:09:22 +0800

    集成单机版 nacos

--- sb3-microcloud/gateway/src/main/java/zzjjcc/gateway/GatewayServerApp.java
+++ sb3-microcloud/gateway/src/main/java/zzjjcc/gateway/GatewayServerApp.java
@@ -2,8 +2,10 @@ package zzjjcc.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayServerApp {
     public static void main(String[] args) {
         SpringApplication.run(GatewayServerApp.class, args);
