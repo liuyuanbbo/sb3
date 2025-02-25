3fa2aa3c2d6f4dbe77aa39af776dbf30769f4fa9
zzjjcc <zheng.jianchao@iwhalecloud.com>
2025-02-25 10:56:35 +0800

    升级 springboot 版本

--- sb3-new-features/src/main/java/org/zmz/sb3/newfeatures/j21/str/STR1.java
+++ sb3-new-features/src/main/java/org/zmz/sb3/newfeatures/j21/str/STR1.java
@@ -2,5 +2,7 @@ package org.zmz.sb3.newfeatures.j21.str;
 
 public class STR1 {
     public static void main(String[] args) {
+        String s="{\"componentId\":\"1644\",\"pageIndex\":1,\"pageSize\":10,\"type\":\"SCHEMA\",\"syncType\":\"HIVE_SCHEMA\",\"language\":\"zh-CN\",\"envType\":\"shanghaidianxin\"}";
+
     }
 }
