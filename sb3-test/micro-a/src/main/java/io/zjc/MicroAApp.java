66252eed5ccc4351bd44d308465a9c59b0553f05
zzjjcc <zheng.jianchao@iwhalecloud.com>
2025-03-30 08:48:34 +0800

    20250330 commit 01

---
+++ sb3-test/micro-a/src/main/java/io/zjc/MicroAApp.java
@@ -0,0 +1,11 @@
+package io.zjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroAApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MicroAApp.class, args);
+    }
+}
--- sb3-test/spring-boot3-test/src/main/java/io/jcz/regex/RegexTest.java
+++ sb3-test/spring-boot3-test/src/main/java/io/jcz/regex/RegexTest.java
@@ -1,12 +1,17 @@
 package io.jcz.regex;
 
+import java.time.Instant;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexTest {
     public static void main(String[] args) {
         String sql = null;
-
+        long epochMilli = Instant.now().toEpochMilli();
+        long i = 1743070387748L - 1743070278374L;
+        // 1800000
+        System.out.println(i);
+        System.out.println(epochMilli);
         System.out.println(isDynamicSql(sql));
     }
 
@@ -14,6 +19,7 @@ public class RegexTest {
         // 检查字符串中是否包含{},且不包括${},其中 {} 中可以包含任意字符
         Pattern pattern = Pattern.compile("\\{(?!(\\$\\{.*}))[^\\{\\}]*\\}");
         Matcher matcher = pattern.matcher(sql);
+
         return matcher.find();
     }
 }
