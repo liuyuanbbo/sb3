114e0ef01ce4613624d9f398318148b3c25cb4e7
jcz <zheng.jianchao@iwhalecloud.com>
2025-11-28 13:56:58 +0800

    删除一些示例demo

--- sb-new-features/src/main/java/org/zmz/sb3/newfeatures/algorithm/feishu/AssertSaveUser.java
+++
@@ -1,8 +0,0 @@
-package org.zmz.sb3.newfeatures.algorithm.feishu;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class AssertSaveUser {
-
-}
--- sb-new-features/src/main/java/org/zmz/sb3/newfeatures/j21/str/STR1.java
+++ sb-new-features/src/main/java/org/zmz/sb3/newfeatures/j21/str/STR1.java
@@ -1,8 +1,22 @@
 package org.zmz.sb3.newfeatures.j21.str;
 
-public class STR1 {
-    public static void main(String[] args) {
-        String s="{\"componentId\":\"1644\",\"pageIndex\":1,\"pageSize\":10,\"type\":\"SCHEMA\",\"syncType\":\"HIVE_SCHEMA\",\"language\":\"zh-CN\",\"envType\":\"shanghaidianxin\"}";
+import com.alibaba.fastjson.JSONObject;
 
+import java.util.Map;
+
+public class STR1 {
+    void main() {
+        String s = """
+                {
+                "componentId":"1644",
+                "pageIndex":1,"pageSize":10,
+                "type":"SCHEMA",
+                "syncType":"HIVE_SCHEMA",
+                "language":"zh-CN",
+                "envType":"shanghaidianxin"
+                }
+                """;
+        Map<?, ?> parseMap = JSONObject.parseObject(s, Map.class);
+        System.out.println(parseMap);
     }
 }
